27dcaaeb5fa8089fc7295b724cc04de46a59ee14
Sang Su Lee <physism@gmail.com>
2021-08-06 00:44:29 +0900

    publish joint state

--- include/gazebo_sim/InvertedPendulumLQR.h
+++ include/gazebo_sim/InvertedPendulumLQR.h
@@ -1,4 +1,18 @@
 #ifndef INVERTED_PENDULUM_LQR_H
 #define INVERTED_PENDULUM_LQR_H
 
+#include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
+
+#include <functional>
+#include <gazebo/common/common.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <ignition/math/Vector3.hh>
+#include <string>
+
+namespace gazebo
+{
+}
+
 #endif
